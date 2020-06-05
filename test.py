6fc3b8113c0250736c0557cc3299e4833f92e95e
MartianLee <martionlee@gmail.com>
2020-06-06 01:58:04 +0900

    Add test file

---
+++ test.py
@@ -0,0 +1,6 @@
+from generator import generate, easy_generate
+
+# Usage
+print(generate(max_length=12, number_length=3, seperator='-'))
+print(generate(max_length=10, number_length=2))
+print(easy_generate())
