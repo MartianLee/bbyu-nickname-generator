b0c378a12c22090fb741d3c4a27ed6491cc89363
MartianLee <martionlee@gmail.com>
2020-06-06 01:55:43 +0900

    Add easy_generate() and seperator parameter

--- generator.py
+++ generator.py
@@ -15,14 +15,21 @@ def generate(max_length, number_length, seperator=None):
             number = ''
         else:
             number = random.randint(pow(10, number_length-1), pow(10, number_length)-1)
-
-        name = adjective[adjective_rand] + animal[animal_rand] + f"{number}"
+        
+        if seperator:
+            name = seperator.join((adjective[adjective_rand], animal[animal_rand], str(number)))
+        else:
+            name = f"{adjective[adjective_rand]}{animal[animal_rand]}{number}"
+        
         if len(name) <= max_length:
             return name
 
-        count = count+1
+        count = count + 1
         if count > 10000:
             raise ValueError('아이디를 생성하지 못하였습니다')
 
+def easy_generate():
+    return generate(max_length=15, number_length=0)
+
 if __name__ == "__main__":
-  print(generate(12, 4))
+    print(easy_generate())
